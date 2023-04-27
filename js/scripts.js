27d51ddfc81d75f8cf39147ea33599ebf277e5d1
Aidan Williams <aidanwilliams01@gmail.com>
2023-04-27 16:20:07 -0700

    group form answers by question and add variable declarations to Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ function onPageLoad() {
   let form = document.querySelector("form");
   function formSubmission(event) {
     event.preventDefault();
+    const q1 = document.querySelector("input[name='q1']:checked").value;
+    const q2 = document.querySelector("input[name='q2']:checked").value;
+    const q3 = document.querySelector("input[name='q3']:checked").value;
+    const q4 = document.querySelector("input[name='q4']:checked").value;
+    const q5 = document.querySelector("input[name='q5']:checked").value;
   }
   form.addEventListener("submit", formSubmission);
 }
