43e89aa42764be48ae97ce27267eb16365ce189a
Aidan Williams <aidanwilliams01@gmail.com>
2023-04-28 14:17:21 -0700

    add branching logic to evaluate form answers and return a result

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,65 @@ function onPageLoad() {
     const q3 = document.querySelector("input[name='q3']:checked").value;
     const q4 = document.querySelector("input[name='q4']:checked").value;
     const q5 = document.querySelector("input[name='q5']:checked").value;
+    let a1 = 0;
+    let a2 = 0;
+    let a3 = 0;
+    if (q1 === "Answer 1") {
+      a1 = 1;
+    }
+    else if (q1 === "Answer 2") {
+      a2 = 1;
+    }
+    else if (q1 === "Answer 3") {
+      a3 = 1;
+    }
+    if (q2 === "Answer 1") {
+      a1 = a1 + 1;
+    }
+    else if (q2 === "Answer 2") {
+      a2 = a2 + 1;
+    }
+    else if (q2 === "Answer 3") {
+      a3 = a3 + 1;
+    }
+    if (q3 === "Answer 1") {
+      a1 = a1 + 1;
+    }
+    else if (q3 === "Answer 2") {
+      a2 = a2 + 1;
+    }
+    else if (q3 === "Answer 3") {
+      a3 = a3 + 1;
+    }
+    if (q4 === "Answer 1") {
+      a1 = a1 + 1;
+    }
+    else if (q4 === "Answer 2") {
+      a2 = a2 + 1;
+    }
+    else if (q4 === "Answer 3") {
+      a3 = a3 + 1;
+    }
+    if (q5 === "Answer 1") {
+      a1 = a1 + 1;
+    }
+    else if (q5 === "Answer 2") {
+      a2 = a2 + 1;
+    }
+    else if (q5 === "Answer 3") {
+      a3 = a3 + 1;
+    }
+    if (a1 >= a2 && a1 >= a3) {
+      document.querySelector("p").innerText = "Coding language 1";
+    }
+    else if (a2 > a1 && a2 >= a3) {
+      document.querySelector("p").innerText = "Coding language 2";
+    }
+    else {
+      document.querySelector("p").innerText = "Coding language 3";
+    }
   }
   form.addEventListener("submit", formSubmission);
 }
 
-window.addEventListener("load", onPageLoad)
+window.addEventListener("load", onPageLoad);
