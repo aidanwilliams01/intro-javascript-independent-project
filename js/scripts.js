ea60dab9459a4edadb4b61642b30cdd989170530
Aidan Williams <aidanwilliams01@gmail.com>
2023-05-01 15:34:40 -0700

    replace if statements in result evaluation with ternaries; adjust concatenation to use literals

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ function onPageLoad() {
       else if (q1 === "Answer 2") {
         a2 = 1;
       }
-      else if (q1 === "Answer 3") {
+      else {
         a3 = 1;
       }
       if (q2 === "Answer 1") {
@@ -33,7 +33,7 @@ function onPageLoad() {
       else if (q2 === "Answer 2") {
         a2 = a2 + 1;
       }
-      else if (q2 === "Answer 3") {
+      else {
         a3 = a3 + 1;
       }
       if (q3 === "Answer 1") {
@@ -42,7 +42,7 @@ function onPageLoad() {
       else if (q3 === "Answer 2") {
         a2 = a2 + 1;
       }
-      else if (q3 === "Answer 3") {
+      else {
         a3 = a3 + 1;
       }
       if (q4 === "Answer 1") {
@@ -51,7 +51,7 @@ function onPageLoad() {
       else if (q4 === "Answer 2") {
         a2 = a2 + 1;
       }
-      else if (q4 === "Answer 3") {
+      else {
         a3 = a3 + 1;
       }
       if (q5 === "Answer 1") {
@@ -60,18 +60,12 @@ function onPageLoad() {
       else if (q5 === "Answer 2") {
         a2 = a2 + 1;
       }
-      else if (q5 === "Answer 3") {
+      else {
         a3 = a3 + 1;
       }
-      if (a1 >= a2 && a1 >= a3) {
-        document.querySelector("p").innerText = name + ", you should study Coding language 1.";
-      }
-      else if (a2 > a1 && a2 >= a3) {
-        document.querySelector("p").innerText = name + ", you should study Coding language 2.";
-      }
-      else {
-        document.querySelector("p").innerText = name + ", you should study Coding language 3.";
-      }
+      document.querySelector("p").innerText = a1 >= a2 && a1 >= a3 ? `${name}, you should study Coding language 1.`
+      : a2 > a1 && a2 >= a3 ? `${name}, you should study Coding language 2.`
+      : `${name}, you should study Coding language 3.`
     }
   }
   form.addEventListener("submit", formSubmission);
